88fd4cfc9ea7d663bcdff36302b7b1363a7b8dac
MoisesD19 <moisesdonayre19@gmail.com>
2024-10-25 01:46:24 -0500

    feat(notifications): add notifications page

---
+++ lib/core/app_constants.dart
@@ -0,0 +1,5 @@
+class AppConstants {
+  static const String baseUrl = 'https://671b21b7acf9aa94f6aca73c.mockapi.io/api/v1';
+
+  static const String notifications = '/notifications';
+}
---
+++ lib/features/notification/data/remote/notification_service.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:safedrive/core/app_constants.dart';
+import 'package:safedrive/features/notification/data/remote/notification_model.dart';
+
+class NotificationService {
+  Future<List<NotificationModel>> getNotifications() async {
+    try {
+      final url = Uri.parse('${AppConstants.baseUrl}${AppConstants.notifications}');
+      http.Response response = await http.get(url);
+
+      print('Status: ${response.statusCode}');
+      print('Body: ${response.body}');
+
+      if (response.statusCode == HttpStatus.ok) {
+        List<dynamic> json = jsonDecode(response.body);
+        return json.map((map) => NotificationModel.fromJson(map)).toList();
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+    return [];
+  }
+}
