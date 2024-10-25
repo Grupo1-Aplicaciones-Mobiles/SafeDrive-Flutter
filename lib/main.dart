a5a22b0f160fdba9c667952665ef23447c9fdeb0
paolomv02 <u202010039@upc.edu.pe>
2024-10-24 23:02:41 -0500

    fix(vehicles): update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:safedrive/features/vehicle/presentation/pages/vehicle_list_page.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,9 +14,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: VehicleListPage(),
       ),
     );
   }
