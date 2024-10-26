27233b1f6535e1d20342c9dca3eadcf5a8ad646b
paolomv02 <u202010039@upc.edu.pe>
2024-10-25 21:09:48 -0500

    fix(vehicles): update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:safedrive/features/vehicle/presentation/pages/vehicle_list_page.dart';
 import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MainApp());
 }
 
